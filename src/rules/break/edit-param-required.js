0c12bb361d94635e8401b767d11a644a881da8a1
Nicolas Fortin <nicolas.fortin37@gmail.com>
2015-12-02 21:06:14 +0100

    Add edit-param-required rule

---
+++ src/rules/break/edit-param-required.js
@@ -0,0 +1,16 @@
+
+export default function addParamRequired({kind, path, lhs, rhs}) {
+  const condition = (kind === 'N' || kind === 'E')
+                 && path.length === 6
+                 && path[0] === 'paths'
+                 && path[3] === 'parameters'
+                 && path[5] === 'required'
+                 && rhs === true;
+  if (condition) {
+    const pathId = path[1];
+    const method = path[2];
+    const paramName = path[4];
+    return `${pathId} (${method}) - Param ${paramName} became required`;
+  }
+  return false;
+}
