ac94548c38ed88237c7358c27981401cf90e815a
zallek <nicolas.fortin37@gmail.com>
2015-12-02 16:55:56 +0100

    Implements smooth rules testing

--- test/rules/rules.spec.js
+++ test/rules/rules.spec.js
@@ -6,15 +6,26 @@ import path from 'path';
 
 import swaggerDiff from '../../src/index';
 import { BREAK_RULES_DIR, SMOOTH_RULES_DIR } from '../../src/constants';
-const breakRules = requireAll(BREAK_RULES_DIR);
-const smootRules = requireAll(SMOOTH_RULES_DIR);
 
 
-describe('Rules', () => {
-  describe('Break', () => {
-    forEach(breakRules, (rule, ruleId) => {
+const RULES_TYPES = [
+  {
+    label: 'Break Rules',
+    rules: requireAll(BREAK_RULES_DIR),
+    testFolder: 'break',
+  },
+  {
+    label: 'Smooth Rules',
+    rules: requireAll(SMOOTH_RULES_DIR),
+    testFolder: 'smooth',
+  },
+];
+
+RULES_TYPES.forEach(({label, rules, testFolder}) => {
+  describe(label, () => {
+    forEach(rules, (rule, ruleId) => {
       describe(ruleId, () => {
-        const dirname = path.join(__dirname, 'break', ruleId);
+        const dirname = path.join(__dirname, testFolder, ruleId);
         if (!fs.existsSync(dirname)) {
           it('should be tested');
           return;
@@ -25,8 +36,8 @@ describe('Rules', () => {
         });
         forEach(outputs, (output, testId) => {
           it('Test ' + testId, (done) => {
-            const oldSpec = require(`./break/${ruleId}/${testId}-old`);
-            const newSpec = require(`./break/${ruleId}/${testId}-new`);
+            const oldSpec = require(`./${testFolder}/${ruleId}/${testId}-old`);
+            const newSpec = require(`./${testFolder}/${ruleId}/${testId}-new`);
             swaggerDiff(oldSpec, newSpec, {skipDiffPostProcessing: true})
               .then(diff => {
                 chai.expect(diff).to.deep.equal(output);
