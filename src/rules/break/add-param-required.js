c56e6a6cd6ae2419fc7759d78f2cffb46f3b7741
zallek <nicolas.fortin37@gmail.com>
2015-12-02 19:10:37 +0100

    Add add-param-required rule

---
+++ src/rules/break/add-param-required.js
@@ -0,0 +1,10 @@
+
+export default function addParamRequired({kind, path, lhs, rhs}) {
+  if (kind === 'N' && path[0] === 'paths' && path[3] === 'parameters' && path.length === 5 && rhs.required === true) {
+    const pathId = path[1];
+    const method = path[2];
+    const paramName = path[4];
+    return `${pathId} (${method}): add required param ${paramName} added`;
+  }
+  return false;
+}
