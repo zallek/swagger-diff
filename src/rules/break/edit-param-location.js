48d2704802f8a48a206ceac6b7777a029c999c9b
Nicolas Fortin <nicolas.fortin37@gmail.com>
2015-12-02 21:27:42 +0100

    Add rule dit-param-location

---
+++ src/rules/break/edit-param-location.js
@@ -0,0 +1,15 @@
+
+export default function editParamType({kind, path, lhs, rhs}) {
+  const match = kind === 'E'
+              && path.length === 6
+              && path[0] === 'paths'
+              && path[3] === 'parameters'
+              && path[5] === 'in';
+  if (match) {
+    const pathId = path[1];
+    const method = path[2];
+    const paramName = path[4];
+    return `${pathId} (${method}) - Param ${paramName} in turn from from ${lhs} to ${rhs}`;
+  }
+  return false;
+}
