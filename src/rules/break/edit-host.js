4546290e74a9590b3b92e353be88840df17c46f4
Nicolas Fortin <nicolas.fortin37@gmail.com>
2016-01-02 15:20:39 +0100

    Add edit-host rule

---
+++ src/rules/break/edit-host.js
@@ -0,0 +1,10 @@
+
+export default function editHost({kind, path, lhs, rhs}) {
+  const match = kind === 'E'
+              && path.length === 1
+              && path[0] === 'host';
+  if (match) {
+    return `Host turned from ${lhs} to ${rhs}`;
+  }
+  return false;
+}
