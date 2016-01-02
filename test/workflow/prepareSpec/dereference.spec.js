af7d267d564ae71548a6cfa7df73ecb52d7d1295
Nicolas Fortin <nicolas.fortin37@gmail.com>
2016-01-02 18:47:41 +0100

    Fix lint issues

--- test/workflow/prepareSpec/dereference.spec.js
+++ test/workflow/prepareSpec/dereference.spec.js
@@ -1,7 +1,3 @@
-import chai from 'chai';
-
-import dereference from '../../../src/workflow/prepareSpec/dereference';
-
 
 describe('Dereference Spec', () => {
   it('must dereference every external definition');
