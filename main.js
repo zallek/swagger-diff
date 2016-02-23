59d4b2f7ce79bae76caac4974558ae6b700aa7ef
Nicolas Fortin <nicolas.fortin37@gmail.com>
2016-02-23 22:11:44 +0100

    Init gh-pages

---
+++ main.js
@@ -0,0 +1,38 @@
+$(function() {
+  $('#compareForm').submit(handleFormSubmit);
+
+  handleQueryStringParams();
+});
+
+function handleQueryStringParams() {
+  var oldSpec = window.location.search.match(/oldSpec=([^&]+)/);
+  var newSpec = window.location.search.match(/newSpec=([^&]+)/);
+
+  if (oldSpec && oldSpec.length > 1) {
+    $('#oldSwaggerSpec').val(decodeURIComponent(oldSpec[1]));
+  }
+
+  if (newSpec && newSpec.length > 1) {
+    $('#newSwaggerSpec').val(decodeURIComponent(newSpec[1]));
+  }
+
+  if (oldSpec && newSpec) {
+    handleFormSubmit();
+  }
+}
+
+function handleFormSubmit(e) {
+  e && e.preventDefault();
+  var oldSpec = $('#oldSwaggerSpec').val();
+  var newSpec = $('#newSwaggerSpec').val();
+
+  if (!oldSpec || !newSpec) return;
+
+  SwaggerDiff(oldSpec, newSpec)
+    .then(function (diff) {
+      $('#diff-result textarea').text(JSON.stringify(diff, null, 2));
+    })
+    .catch(function(err) {
+      $('#diff-result textarea').text(err.message);
+    });
+}
