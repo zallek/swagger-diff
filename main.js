51c1705d8757702eb43cb0bd1ea059abadbdc79a
Nicolas Fortin <nicolas.fortin37@gmail.com>
2016-02-28 21:45:37 +0100

    Update querystring on submit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('#compareForm').submit(handleFormSubmit);
+  $('#compareForm').submit(startCompare);
 
   handleQueryStringParams();
 });
@@ -9,24 +9,29 @@ function handleQueryStringParams() {
   var newSpec = window.location.search.match(/newSpecUrl=([^&]+)/);
 
   if (oldSpec && oldSpec.length > 1) {
-    $('#oldSwaggerSpec').val(decodeURIComponent(oldSpec[1]));
+    $('#oldSpecUrl').val(decodeURIComponent(oldSpec[1]));
   }
 
   if (newSpec && newSpec.length > 1) {
-    $('#newSwaggerSpec').val(decodeURIComponent(newSpec[1]));
+    $('#newSpecUrl').val(decodeURIComponent(newSpec[1]));
   }
 
-  if (oldSpec && newSpec) {
-    handleFormSubmit();
-  }
+  startCompare();
 }
 
-function handleFormSubmit(e) {
+function startCompare(e) {
   e && e.preventDefault();
-  var oldSpec = $('#oldSwaggerSpec').val();
-  var newSpec = $('#newSwaggerSpec').val();
+  var oldSpec = $('#oldSpecUrl').val();
+  var newSpec = $('#newSpecUrl').val();
 
   if (!oldSpec || !newSpec) return;
+
+  try {
+    setQueryStringParam([
+      ['oldSpecUrl', oldSpec],
+      ['newSpecUrl', newSpec]
+    ]);
+  } catch (e) {}
 
   SwaggerDiff(oldSpec, newSpec)
     .then(function (diff) {
@@ -35,4 +40,13 @@ function handleFormSubmit(e) {
     .catch(function(err) {
       $('#diff-result textarea').text(err.message);
     });
+}
+
+function setQueryStringParam(params) {
+  var url = window.location.href;
+  params.forEach(function (param, i) {
+    url = url.replace(new RegExp(param[0] + '=[^&]+'), '');
+    url += (!window.location.search && i === 0 ? '?' : '&') + param[0] + '=' + encodeURIComponent(param[1]);
+  });
+  history.pushState({}, '', url);
 }
