dbf290e500fddc65653344f242337174b3d64888
zallek <nicolas.fortin37@gmail.com>
2016-03-22 18:08:26 +0100

    Make rule edit-description catch more description changes

--- src/rules/smooth/edit-description.js
+++ src/rules/smooth/edit-description.js
@@ -1,9 +1,8 @@
 
 export default function editDescription({ kind, path, lhs, rhs }) {
   const match = kind === 'E'
-              && path.length === 4
-              && path[0] === 'paths'
-              && path[3] === 'description';
+              && path.length >= 4
+              && path[path.length - 1] === 'description';
   if (match) {
     const pathId = path[1];
     const method = path[2];
