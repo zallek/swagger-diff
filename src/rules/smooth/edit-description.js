ca17ca5135600acc5cc198433008292700d5565d
zallek <nicolas.fortin37@gmail.com>
2016-11-18 15:47:24 +0100

    Make edit-description handle any kind of description edition

--- src/rules/smooth/edit-description.js
+++ src/rules/smooth/edit-description.js
@@ -1,15 +1,13 @@
 
 export default function editDescription({ kind, path, lhs, rhs }) {
   const match = kind === 'E'
-              && path.length >= 4
+              && path.length >= 2
               && path[path.length - 1] === 'description';
   if (match) {
-    const pathId = path[1];
-    const method = path[2];
+    const p = `/${path.slice(0, -1).join('/')}/`;
     return {
-      message: `${pathId} (${method}) - Description turned from ${lhs} to ${rhs}`,
-      path: pathId,
-      method,
+      message: `${p} - Description turned from ${lhs} to ${rhs}`,
+      descriptionPath: p,
       previousDescription: lhs,
       currentDescription: rhs,
     };
