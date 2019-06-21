ecfb82a903a9ee4306f1eb1f7afe223de996ef10
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2019-06-21 22:43:59 +0300

    fix lint prettier issues

--- src/rules/smooth/delete-description.js
+++ src/rules/smooth/delete-description.js
@@ -1,11 +1,11 @@
 export default function deleteDescription({ kind, path, lhs, rhs }) {
-  const match = kind === "D" && path.length >= 2 && path[path.length - 1] === "description";
+  const match = kind === 'D' && path.length >= 2 && path[path.length - 1] === 'description';
   if (match) {
-    const p = `/${path.slice(0, -1).join("/")}/`;
+    const p = `/${path.slice(0, -1).join('/')}/`;
     return {
       message: `${p} - Description deleted: ${lhs}`,
       descriptionPath: p,
-      description: lhs
+      description: lhs,
     };
   }
   return false;
